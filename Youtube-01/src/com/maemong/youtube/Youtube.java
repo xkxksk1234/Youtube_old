fbc849da61cfc4740740d12d92d0b14b414c00fe
Maemong <xkxksk0123@gmail.com>
2017-06-24 14:42:28 +0900

    Youtube 01 upload

---
+++ Youtube-01/src/com/maemong/youtube/Youtube.java
@@ -0,0 +1,10 @@
+package com.maemong.youtube;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Youtube extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		System.out.println("플러그인이 활성화되었습니다.");
+	}
+}
